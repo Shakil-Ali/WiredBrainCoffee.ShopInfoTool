31f417de85504a1f9b4748d3b26847216be46353
Shakil Ali <shakil_419@hotmail.co.uk>
2024-03-11 14:51:23 +0000

    Added coffee shop data provider

---
+++ WiredBrainCoffee.ShopInfoTool/Data/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using WiredBrainCoffee.ShopInfoTool.Model;
+
+namespace WiredBrainCoffee.ShopInfoTool.Data;
+
+public class CoffeeShopDataProvider
+{
+    public IEnumerable<CoffeeShop> LoadShops()
+    {
+        return new CoffeeShop[]
+        {
+            new CoffeeShop("London", 45),
+            new CoffeeShop("Sylhet", 39),
+            new CoffeeShop("Medina", 28)
+        };
+    }
+}
